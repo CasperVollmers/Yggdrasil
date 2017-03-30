5f0b91c0283d6386874898d6b5898ac9c3ace3ba
CasperVollmers <mercedesbenz>
2017-03-30 22:54:44 +0200

    Unity_Files

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item: MonoBehaviour {
+
+	public int distanceToItem;
+
+	void Start () {
+
+	}
+
+
+
+	void Update ()	{
+
+		Collect();
+
+		}
+
+	void Collect(){
+
+		if (Input.GetMouseButtonUp(1)){
+
+			RaycastHit hit;
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+
+			if(Physics.Raycast(ray, out hit, distanceToItem)){
+				if(hit.collider.gameObject.name == "Gyl") {
+
+					Debug.Log("ITEM HIT");
+
+					Destroy(hit.collider.gameObject);
+				}
+			}
+		}	
+	}
+}
